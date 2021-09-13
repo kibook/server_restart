41d5ab6168853b0254f7c943ddd9e9f08a9b2f61
kibukj <kibuzhj@gmail.com>
2021-09-13 16:53:01 -0400

    Separate dependencies in manifest
    The "dependencies" metadata does not work with natives like
    GetNumResourceMetadata/GetResourceMetadata, probably because it is in
    irregular plural.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,10 +2,8 @@ game 'rdr3'
 fx_version 'adamant'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-dependencies {
-	'weathersync',
-	'events'
-}
+dependency 'weathersync' -- https://github.com/kibook/redm-weathersync
+dependency 'events'      -- https://github.com/kibook/redm-events
 
 server_only 'yes'
 
